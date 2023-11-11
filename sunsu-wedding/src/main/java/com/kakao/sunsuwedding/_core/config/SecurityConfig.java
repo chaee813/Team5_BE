55dc588aa5a168762e55c9e60f6183fece5582ad
Youngchae Cheon <82564901+chaee813@users.noreply.github.com>
2023-11-11 16:03:24 +0900

    Update SecurityConfig.java

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/_core/config/SecurityConfig.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/_core/config/SecurityConfig.java
@@ -144,9 +144,10 @@ public class SecurityConfig {
         configuration.addAllowedMethod(HttpMethod.PUT);
         configuration.addAllowedMethod(HttpMethod.DELETE);
 
-        configuration.addAllowedOriginPattern("http://localhost:3000");
-        configuration.addAllowedOriginPattern("https://k6f3d3b1a0696a.user-app.krampoline.com");
-        configuration.addAllowedOriginPattern("https://k5c1813d97f50a.user-app.krampoline.com"); // 프론트 테스트용
+        configuration.addAllowedOriginPattern("*");
+        //configuration.addAllowedOriginPattern("http://localhost:3000");
+        //configuration.addAllowedOriginPattern("https://k6f3d3b1a0696a.user-app.krampoline.com");
+        //configuration.addAllowedOriginPattern("https://k5c1813d97f50a.user-app.krampoline.com"); // 프론트 테스트용
 
         configuration.setAllowCredentials(true);
 
